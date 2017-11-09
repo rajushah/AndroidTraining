abcad00192b8ac4226bdea2e776a39ebc6ce4ff1
rajesh khadka <rajeshkhadka1993@gmail.com>
2017-11-09 08:32:58 +0545

    launch movie activity

--- app/src/main/java/com/halftone/movie/MovieActivity.java
+++ app/src/main/java/com/halftone/movie/MovieActivity.java
@@ -1,6 +1,8 @@
 package com.halftone.movie;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -10,5 +12,10 @@ public class MovieActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+    }
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, MovieActivity.class);
+        context.startActivity(starter);
     }
 }
