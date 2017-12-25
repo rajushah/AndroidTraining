cfcb20deea1c2743fb23167a522413f77d38992c
rajesh khadka <rajesh.smartmobe@gmail.com>
2017-12-25 08:24:34 +0545

    add fragment

--- app/src/main/java/com/halftone/LoginFragment.java
+++ app/src/main/java/com/halftone/LoginFragment.java
@@ -2,10 +2,16 @@ package com.halftone;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.halftone.movie.R;
 
@@ -14,6 +20,8 @@ import com.halftone.movie.R;
  */
 public class LoginFragment extends Fragment {
 
+    TextInputLayout mTilUsername;
+    TextInputLayout mTilPassword;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -23,8 +31,33 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        mTilUsername = view.findViewById(R.id.username);
+        mTilPassword = view.findViewById(R.id.password);
+
+
+        mTilUsername.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Toast.makeText(getActivity(), "changed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
